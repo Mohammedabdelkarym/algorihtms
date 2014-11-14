acf7d781fe33c035a21004843fcc970ec90eab15
mohammedabdelkarym <mohammed.abdelkarym@gmail.com>
2014-11-13 18:29:55 -0600

    no message

---
+++ Algorithms/Algorithms/QuickFind.cpp
@@ -0,0 +1,23 @@
+//
+//  QuickFind.cpp
+//  Algorithms
+//
+//  Created by Mohammed Abdelkarym on 11/13/14.
+//  Copyright (c) 2014 mo7amedkhairy. All rights reserved.
+//
+
+class QuickFind
+{
+
+private:
+    int numberOfObjects;
+
+public:
+    QuickFind(int n)
+    {
+        numberOfObjects=n;
+    }
+    
+    
+    
+};
